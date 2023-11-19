38e39520a9c52dacc61341758ffa73e711d15e7f
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-11-19 01:18:04 -0300

    +:more about functions

---
+++ LearnToProgram/Chapter05/angry_boss.rb
@@ -0,0 +1,5 @@
+if __FILE__ == $0
+	puts("WHAT THE FUCK YOU WANT?")
+	response = gets.chomp
+	puts("WHAT? YOU WANT #{response.upcase}? FUCK YOU")
+end
---
+++ LearnToProgram/Chapter05/index.rb
@@ -0,0 +1,6 @@
+if __FILE__ == $0
+	puts("Table index".center(42))
+	puts("Intro".ljust(21) + "pag. 2".rjust(21))
+	puts("Mid".ljust(21) + "pag. 12".rjust(21))
+	puts("End".ljust(21) + "pag. 21".rjust(21))
+end
