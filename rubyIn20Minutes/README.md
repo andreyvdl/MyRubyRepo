8f10ca4978983af85f6caab06b0a65abc16f85a8
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-11-18 21:09:18 -0300

    +F:added readme and fixed in indentation

---
+++ rubyIn20Minutes/README.md
@@ -0,0 +1,3 @@
+# Ruby in 20 Minutes
+
+Code based on the [Ruby in 20 Minutes](https://www.ruby-lang.org/en/documentation/quickstart/) page on the official Ruby website.
--- rubyIn20Minutes/rubyIn20Minutes.rb
+++ rubyIn20Minutes/rubyIn20Minutes.rb
@@ -1,47 +1,47 @@
 #!/usr/bin/env ruby
 
 class MegaAnfitriao
-    attr_accessor :names
+	attr_accessor :names
 
-    def initialize(names = "World")
-        @names = names
-    end
-    def say_hi
-        if @names.nil?
-            puts "..."
-        elsif @names.respond_to?("each")
-            @names.each do |name|
-                puts "Hello #{name}"
-            end
-        else
-            puts "Hello #{@names}"
-        end
-    end
-    def say_bye
-        if @names.nil?
-            puts "..."
-        elsif @names.respond_to?("join")
-            puts "Goodbye #{@names.join(", ")}. Come back anytime."
-        else
-            puts "Goodbye #{names}. Come back anytime."
-        end
-    end
+	def initialize(names = "World")
+		@names = names
+	end
+	def say_hi
+		if @names.nil?
+			puts "..."
+		elsif @names.respond_to?("each")
+			@names.each do |name|
+				puts "Hello #{name}"
+			end
+		else
+			puts "Hello #{@names}"
+		end
+	end
+	def say_bye
+		if @names.nil?
+			puts "..."
+		elsif @names.respond_to?("join")
+			puts "Goodbye #{@names.join(", ")}. Come back anytime."
+		else
+			puts "Goodbye #{names}. Come back anytime."
+		end
+	end
 end
 
 if __FILE__ == $0
-    mg = MegaAnfitriao.new
-    mg.say_hi
-    mg.say_bye
+	mg = MegaAnfitriao.new
+	mg.say_hi
+	mg.say_bye
 
-    mg.names = "Diogo"
-    mg.say_hi
-    mg.say_bye
+	mg.names = "Diogo"
+	mg.say_hi
+	mg.say_bye
 
-    mg.names = ["Alberto", "Beatriz", "Carlos", "David", "Ernesto"]
-    mg.say_hi
-    mg.say_bye
+	mg.names = ["Alberto", "Beatriz", "Carlos", "David", "Ernesto"]
+	mg.say_hi
+	mg.say_bye
 
-    mg.names = nil
-    mg.say_hi
-    mg.say_bye
+	mg.names = nil
+	mg.say_hi
+	mg.say_bye
 end
